c54d3948adbe312da3b5c2357f721340f53a7d4c
Yuri Ksenofontov <yuran_@mail.ru>
2016-07-08 23:18:39 +0400

    add grabber

--- src/main/java/Informator.java
+++ src/main/java/Informator.java
@@ -7,7 +7,9 @@ public class Informator {
 
     public Informator(){
         ArrayList<String> params = new ArrayList<String>();
+//        ("OS", "IE", "Chrome", "Firefox", "Java", "NET")
         params.add("OS");
+        params.add("IE");
         String pcData = PCDataGrabber.getInstance().getPCdata(params);
         ClipboardAccess.getInstance().copyToClipboard(pcData);
     }
--- src/main/java/PCDataGrabber.java
+++ src/main/java/PCDataGrabber.java
@@ -1,4 +1,6 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 import static com.sun.corba.se.spi.activation.IIOP_CLEAR_TEXT.value;
@@ -24,21 +26,53 @@ public class PCDataGrabber {
     public static ArrayList<String> getGrabbedData() {
         return grabbedData;
     }
-    private final static String OS_SCRIPT = "ver";
+
+    private final static String JAVA_SCRIPT = "java -version";
+
     private void setGrabbedData(String grabbedData) {
         PCDataGrabber.grabbedData.add(grabbedData);
     }
 
-    public String getPCdata(ArrayList<String> params){
-        String result="PC Configuration:\n";
+    public String getPCdata(ArrayList<String> params) {
+        String result = "PC Configuration:\n";
         for (String param : params) {
             if (param.equals("OS")) result = result + "OS version: " + getOSVersion() + "\n";
 
         }
-      return result;
+        return result;
     }
 
-    private String getOSVersion(){
+    private String getOSVersion() {
         return System.getProperty("os.name");
+    }
+
+    private ArrayList<String> getJavaVersion() {
+        return executeScript(JAVA_SCRIPT);
+    }
+
+    private ArrayList<String> executeScript(String commands) {
+        Runtime rt = Runtime.getRuntime();
+        Process proc = null;
+        try {
+            proc = rt.exec(commands);
+        } catch (IOException e) {
+            System.out.println("Can not execute script = " + commands);
+        }
+        BufferedReader stdInput = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+        BufferedReader stdError = new BufferedReader(new InputStreamReader(proc.getErrorStream()));
+        System.out.println("Here is the standard output of the command:\n");
+        String s;
+        ArrayList<String> result = new ArrayList<String>();
+        try {
+            int i = 0;
+            while ((s = stdInput.readLine()) != null) {
+                result.add(s);
+                i = i + 1;
+                System.out.println(s);
+            }
+        } catch (IOException e) {
+            System.out.println("Can not get output of script = " + commands);
+        }
+        return result;
     }
 }
