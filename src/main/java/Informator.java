62b39b377562b817b5e667ddd0712b8f53e7ff2f
Andrey <lrocrul@gmail.com>
2016-07-10 13:15:07 +0300

    load data in other thread on start. need change PCDataGrabber: -getPCData() write all data in "private static ArrayList<String> grabbedData;" and return nothing; -new function to make string from ArrayList<String> params,where params-"Java","OS" etc. this function return same string as getPCData() now;

--- src/main/java/Informator.java
+++ src/main/java/Informator.java
@@ -9,30 +9,32 @@ import java.util.ArrayList;
  * Created by yksenofontov on 08.07.2016.
  */
 public class Informator extends JFrame {
-    int left,top,dy,dx;
-    boolean pressed;
+    int left, top, dy, dx;
+    boolean pressed, ready;
     JPopupMenu menu;
     ArrayList<String> params;
+    String gr;///////////////////////////////////
+
     public Informator() {
         setLayout(null);
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        left = (int)screenSize.getWidth();
-        top = (int)screenSize.getHeight();
-        setLocation( left-40,  top-80);
+        left = (int) screenSize.getWidth();
+        top = (int) screenSize.getHeight();
+        setLocation(left - 40, top - 80);
         setSize(30, 30);
         setUndecorated(true);
         setBackground(new Color(0, 0, 0, 0));
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        JLabel jl = new JLabel(new ImageIcon(new ImageIcon("resources\\images.png").getImage().getScaledInstance(30, 30, java.awt.Image.SCALE_SMOOTH)));
+        final JLabel jl = new JLabel(new ImageIcon(new ImageIcon("resources\\reload.png").getImage().getScaledInstance(30, 30, java.awt.Image.SCALE_SMOOTH)));
         jl.setVisible(true);
-        jl.setBounds(0,0,30,30);
+        jl.setBounds(0, 0, 30, 30);
         JPanel jp = new JPanel(null);
         jp.setVisible(true);
-        jp.setBounds(0,0,30,30);
+        jp.setBounds(0, 0, 30, 30);
         jp.setOpaque(false);
         jp.add(jl);
         params = new ArrayList<String>();
-        for (String pr:Properties.allProperties){
+        for (String pr : Properties.allProperties) {
             if (Properties.getInstance().getUserProperties().contains(pr)) params.add(pr);
         }
         jp.addMouseListener(new MyMouseAdapter());
@@ -43,7 +45,7 @@ public class Informator extends JFrame {
         menu = new JPopupMenu();
         JMenuItem menuItem;
         JCheckBoxMenuItem cbMenuItem;
-        for (String pr:Properties.allProperties) {
+        for (String pr : Properties.allProperties) {
             cbMenuItem = new JCheckBoxMenuItem(pr);
             if (params.contains(pr)) cbMenuItem.setSelected(true);
             else cbMenuItem.setSelected(false);
@@ -52,61 +54,75 @@ public class Informator extends JFrame {
             menu.add(cbMenuItem);
         }
         menuItem = new JMenuItem("Exit");
-        menuItem.addActionListener(new ActionListener()  {
+        menuItem.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 System.exit(0);
             }
         });
         menu.add(menuItem);
+        Thread thread = new Thread(new Runnable() {
+            public void run() {
+                gr = PCDataGrabber.getInstance().getPCdata(params);///Изменить(нужен метод в PCDataGrabber)
+                ready = true;
+                jl.setIcon(new ImageIcon(new ImageIcon("resources\\images.png").getImage().getScaledInstance(30, 30, java.awt.Image.SCALE_SMOOTH)));
+            }
+        });
+        thread.start();
     }
-    private class CBActionListener implements ActionListener{
-        public void actionPerformed(ActionEvent e){
+
+    private class CBActionListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
             menu.setVisible(true);
-            if (((AbstractButton)e.getSource()).getModel().isSelected()) params.add(e.getActionCommand());
+            if (((AbstractButton) e.getSource()).getModel().isSelected()) params.add(e.getActionCommand());
             else params.remove(e.getActionCommand());
             Properties.getInstance().setUserProperties(params);
         }
     }
 
-    class MyMouseMotionAdapter extends MouseMotionAdapter{
-        public void mouseDragged(MouseEvent e){
-            if (!pressed)return;
-            top=top+(e.getY()-dy);
-            left=left+(e.getX()-dx);
-            setLocation(left,top);
+    class MyMouseMotionAdapter extends MouseMotionAdapter {
+        public void mouseDragged(MouseEvent e) {
+            if (!pressed) return;
+            top = top + (e.getY() - dy);
+            left = left + (e.getX() - dx);
+            setLocation(left, top);
         }
     }
 
-    class MyMouseAdapter extends MouseAdapter{
-        public void mousePressed(MouseEvent e){
-            dy=e.getY();
-            dx=e.getX();
-            pressed=true;
+    class MyMouseAdapter extends MouseAdapter {
+        public void mousePressed(MouseEvent e) {
+            dy = e.getY();
+            dx = e.getX();
+            pressed = true;
         }
 
-        public void mouseReleased(MouseEvent e){
-            pressed=false;
+        public void mouseReleased(MouseEvent e) {
+            pressed = false;
         }
 
         public void mouseClicked(MouseEvent e) {
             ///1-left,3-right
-            if (e.getButton()==1){
-                String t = PCDataGrabber.getInstance().getPCdata(params);//error
-                ClipboardAccess.getInstance().copyToClipboard(t);
-            }
-            else if (e.getButton()==3){
+            if (e.getButton() == 1) {
+                if (ready) ClipboardAccess.getInstance().copyToClipboard(gr);
+                else {
+                    JComponent component = (JComponent)e.getSource();
+                    component.setToolTipText("Data isn't ready");
+                    MouseEvent phantom = new MouseEvent(
+                            component,
+                            MouseEvent.MOUSE_MOVED,
+                            System.currentTimeMillis(),
+                            0,
+                            0,
+                            0,
+                            0,
+                            false);
+                    ToolTipManager.sharedInstance().mouseMoved(phantom);
+                }
+            } else if (e.getButton() == 3) {
                 menu.setVisible(true);
-                menu.show(e.getComponent(),e.getX(),e.getY());
+                menu.show(e.getComponent(), e.getX(), e.getY());
             }
         }
     }
-//
-//    readProperties
-//
-//    saveProperties
-//     getPCData
-//
-//    CopyToClipboard
 }
 
 class StayOpenCheckBoxMenuItemUI extends BasicCheckBoxMenuItemUI {
