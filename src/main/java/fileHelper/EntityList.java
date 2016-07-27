5c2ddf9d50fd09eaa9cc48ea90a66f0e3d2ecc54
Yuri Ksenofontov <yuran_@mail.ru>
2016-07-27 23:14:08 +0400

    ad throws ex add clean implementation

--- src/main/java/fileHelper/EntityList.java
+++ src/main/java/fileHelper/EntityList.java
@@ -12,19 +12,19 @@ import java.util.List;
 public class EntityList {
     private final static Logger logger = Logger.getLogger(EntityList.class);
     private List<Entity> entities = new ArrayList<Entity>();
+
     public List<Entity> getEntities() {
         return entities;
     }
 
-    public void addEntity(String name, String link) throws Exception{
+    public void addEntity(String name, String link) throws Exception {
         File file = new File(link);
         if (file.exists()) {
             entities.add(new Entity(name, link));
-        }
-            else{
+        } else {
             throw new Exception("Path doesn't exist");
         }
-       logger.info("New entity successfully added.");
+        logger.info("New entity successfully added.");
     }
 
     public void removeEntity(Entity ent) {
@@ -32,14 +32,13 @@ public class EntityList {
         logger.info("Entity successfully removed.");
     }
 
-    public void updateEntity(Entity ent,String newName,String newLink) throws Exception{
+    public void updateEntity(Entity ent, String newName, String newLink) throws Exception {
         File file = new File(newLink);
         if (file.exists()) {
             int index = entities.indexOf(ent);
-            entities.set(index,new Entity(newName,newLink));
+            entities.set(index, new Entity(newName, newLink));
             logger.info("Entity successfully updated.");
-        }
-        else{
+        } else {
             throw new Exception("Path doesn't exist");
         }
     }
